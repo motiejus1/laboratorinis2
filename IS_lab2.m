0d2967148a5eeea7685c928fdacf44a850f255e4
Motiejus <motiejus.baliukonis@vka.lt>
2019-12-19 20:40:25 +0200

    *pridetas svoriu resetinimas *pridetas iteraciju skaiciavimas

--- IS_lab2.m
+++ IS_lab2.m
@@ -2,14 +2,14 @@ clear;
 clc;
 
 %4 neuronu daugiasluoksnis perceptronas 1 pirmame sluoksnyje, 2 pasleptame, 1 treciame
-%keiciam
+
 
 %funkcijos x
 x = 1/20:1/20:1;
 %originali funkcijos reiksme
 d =(1 + 0.6*sin((2*pi*x)/0.7)+0.3*sin(2*pi*x))/2;
 %Zingsinis
-et = 0.2;
+et = 0.3;
 %neuronu svoriai ir b kintamieji. visi parenkami random iki 1. Pakoreguotas
 %numeravimas pagal paskaitos uzrasus, nes blogai skaiciavo koeficientus
 
@@ -29,21 +29,30 @@ w_23 = randn(1);
 w_24 = randn(1);
 
 b_21 = randn(1);
+
+%isirasom koeficientus i masyva jei geri duomenys
+A = [w_11 w_12 w_13 w_14 b_11 b_12 b_13 b_14 w_21 w_22 w_23 w_24 b_21 ]
+
 %apsirasome pradinius duomenis
 eklaida = 1;
+
+preklaida = 0;
 %naudojama cikle klaidos perkaiciavimui. klaidos skaiciavima keiciam i
 %masyva, tiek kiek yra x, keiciam zymejima
 e = zeros(1,length(x));
 
 %perceptrono isvesties masyvas
 y = zeros(1,length(x));
-
+%iteraciju skaitiklis
 i = 0;
-    while eklaida > 0.00002
+    %toks skaicius nes ties 0.0000002135 pasiekiamas arciausias rezultatas,
+    %%0.0000014858 kitas skaicius
+    %vidutine grafiko kvadratine paklaida 0.0032049894  
+    while eklaida > 0.0000002145                 %0.0000014859
+                    
         %kadangi x reiksmiu vektorius reikia atlikti perskaiciavimus su visais
-        %x
+        i = i+1;
         for iterator = 1:1:length(x)
-            i = i+1;
             %keiciame skaiciavimo logika pagal paskaitos uzrasus, blogi
             %koeficientai. Neuronu isvestys
             y1 = 1/exp(-x(iterator)*w_11-b_11);
@@ -90,71 +99,45 @@ i = 0;
             eklaida = abs(e(iterator));
             
         end
+        preklaida = eklaida;
+        %jei per n iteraciju nepasiekiamas norimas tikslumas,
+        %atnaujinami svorio koeficientai
+        if(i==50000)
+            w_11 = randn(1);
+            w_12 = randn(1);
+            w_13 = randn(1);
+            w_14 = randn(1);
+
+            b_11 = randn(1);
+            b_12 = randn(1);
+            b_13 = randn(1);
+            b_14 = randn(1);
+
+            w_21 = randn(1);
+            w_22 = randn(1);
+            w_23 = randn(1);
+            w_24 = randn(1);
+
+            b_21 = randn(1);
+            i=0;
+        end    
+        %isvedimas klaidos. Naudojama paskutine x reiksmes klaida.
+        %isvedamas iteraciju skaicius
+        fprintf('e1 = %2.10f iteracija = %2f   \n',eklaida, i);
         
-        %isvedimas klaidos. Naudojama paskutine x reiksmes klaida
-        fprintf('e1 = %2.10f   \n',eklaida);
-          %skaiciuojamas kvadratinis vidurkis klaidu  
-%err1 = ekopija;
-        %  err = rms(isejimoklaidumasyvas);
-          
-         % if (abs(err-ekopija) < 0.00000001)
-            
-         %   w_11_1 = randn(1);
-         %   w_21_1 = randn(1);
-         %   b_1_1 = randn(1);
-        %    b_2_1 = randn(1);
-
-       %    w_11_2 = randn(1);
-       %    w_21_2 = randn(1);
-        %    w_12_2 = randn(1);
-        %    w_22_2 = randn(1);
-       %     b_1_2 = randn(1);
-        %    b_2_2 = randn(1);
-
-        %    w_11_3 = randn(1);
-        %    w_12_3 = randn(1);
-        %    b_1_3 = randn(1);
-
-        %    w_11_4 = randn(1);
-          %  b_1_4 = randn(1);
-         
-     %   end
-        
-      %  if(abs(err-ekopija) < 0.000000001)  
-       %      w_11_1=w_11_1+rand(1)/8;
-       %     w_21_1=w_21_1+rand(1)/8;
-        %    b_1_1=b_1_1+rand(1)/8;
-        %    b_2_1=b_2_1+rand(1)/8;
-         %   w_11_2=w_11_2+rand(1)/8;
-        %    w_21_2=w_21_2+rand(1)/8;
-          %  w_12_2=w_12_2+rand(1)/8;
-         %   w_22_2=w_22_2+rand(1)/8;
-         %   b_1_2=b_1_2+rand(1)/8;
-         %   b_2_2=b_2_2+rand(1)/8;
-        %    w_11_3=w_11_3+rand(1)/8;
-         %   w_12_3=w_12_3+rand(1)/8;
-         %   b_1_3=b_1_3+rand(1)/8;
-         %   w_11_4=w_11_4+rand(1)/8;
-         %   b_1_4=b_1_4+rand(1)/8;
-      %  end  
-      
-      %  ekopija = err;
-      %  errbraizymui(i) = err; 
-       %      figure(1);
-         %    plot(errbraizymui)
-          
-      %plot(aproksx,aproksy)
       
       
 
     end
+
+
     %pasibaigus ciklui kai pasiekiams norimas tikslumas.nubraizomas
     plot(y);
     hold on
     plot(d);
     legend('aproksimuota kreive', 'originali kreive');
     %apskaiciuojama vidutine kvadratine paklaida
-    immse(y,d)
+    fprintf('vidutine kvadratine paklaida = %2.10f   \n',immse(y,d));
 
 
 
